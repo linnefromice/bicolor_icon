7d66c063dff108b0758d322caabd5bffa8b787be
linnefromice <linnefromice@gmail.com>
2021-03-02 23:35:48 +0900

    modify widget - set required & add alignments map

--- lib/bicolor_icon.dart
+++ lib/bicolor_icon.dart
@@ -1,28 +1,29 @@
-// You have generated a new plugin project without
-// specifying the `--platforms` flag. A plugin project supports no platforms is generated.
-// To add platforms, run `flutter create -t plugin --platforms <platforms> .` under the same
-// directory. You can also find a detailed instruction on how to add platforms in the `pubspec.yaml` at https://flutter.dev/docs/development/packages-and-plugins/developing-packages#plugin-platforms.
-
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+
+final Map<Alignment, Alignment> _reverseAlignment = {
+  Alignment.topLeft: Alignment.bottomRight,
+  Alignment.topCenter: Alignment.bottomCenter,
+  Alignment.topRight: Alignment.bottomLeft,
+  Alignment.centerLeft: Alignment.centerRight,
+  Alignment.centerRight: Alignment.centerLeft,
+  Alignment.bottomLeft: Alignment.topRight,
+  Alignment.bottomCenter: Alignment.topCenter,
+  Alignment.bottomRight: Alignment.topLeft,
+};
 
 class BicolorIcon extends StatelessWidget {
   BicolorIcon({
-    this.iconData,
-    this.iconSize,
-    this.rate,
-    this.beginAlignment,
-    this.endAlignment,
-    this.beginColor,
-    this.endColor
+    @required this.iconData,
+    @required this.iconSize,
+    @required this.rate,
+    @required this.beginAlignment,
+    @required this.beginColor,
+    @required this.endColor
   });
   final IconData iconData;
   final double iconSize;
   final double rate;
   final Alignment beginAlignment;
-  final Alignment endAlignment;
   final Color beginColor;
   final Color endColor;
 
@@ -31,7 +32,7 @@ class BicolorIcon extends StatelessWidget {
     return ShaderMask(
       shaderCallback: (bounds) => LinearGradient(
         begin: beginAlignment,
-        end: endAlignment,
+        end: _reverseAlignment[beginAlignment],
         colors: [beginColor, endColor],
         stops: [rate, rate],
         tileMode: TileMode.mirror,
