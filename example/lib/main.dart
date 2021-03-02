f670c393a9f770215e25efcc1861c85616a47f91
linnefromice <linnefromice@gmail.com>
2021-03-02 23:58:33 +0900

    update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,58 +1,142 @@
-import 'package:flutter/material.dart';
-import 'dart:async';
+import 'dart:math';
 
-import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
 import 'package:bicolor_icon/bicolor_icon.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(App());
 }
 
-class MyApp extends StatefulWidget {
+class App extends StatefulWidget {
   @override
-  _MyAppState createState() => _MyAppState();
+  _State createState() => _State();
 }
 
-class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
+class _State extends State<App> {
+  double _currentSliderValue = 50;
 
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
+  Widget _buildFavorite({ @required final double iconSize, @required final double rate }) => BicolorIcon(
+    iconData: Icons.favorite,
+    iconSize: iconSize,
+    rate: rate,
+    beginAlignment: Alignment.centerLeft,
+    beginColor: Colors.pink,
+    endColor: Colors.white
+  );
+
+  Widget _buildFavorites() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _buildFavorite(iconSize: 40, rate: 0.3),
+        _buildFavorite(iconSize: 50, rate: 0.5),
+        _buildFavorite(iconSize: 60, rate: 0.7),
+      ],
+    );
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await BicolorIcon.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
+  Widget _buildStar({ @required final double iconSize, @required final double rate }) => BicolorIcon(
+    iconData: Icons.star,
+    iconSize: iconSize,
+    rate: rate,
+    beginAlignment: Alignment.bottomCenter,
+    beginColor: Colors.yellow,
+    endColor: Colors.white
+  );
 
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
+  Widget _buildStars() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _buildStar(iconSize: 20, rate: 0.2),
+        _buildStar(iconSize: 30, rate: 0.5),
+        _buildStar(iconSize: 40, rate: 0.8),
+      ],
+    );
+  }
 
-    setState(() {
-      _platformVersion = platformVersion;
-    });
+  Widget _buildCreditCard({ @required final double iconSize, @required final double rate }) => BicolorIcon(
+    iconData: Icons.credit_card,
+    iconSize: iconSize,
+    rate: rate,
+    beginAlignment: Alignment.topLeft,
+    beginColor: Colors.green,
+    endColor: Colors.blue
+  );
+
+  Widget _buildCreditCards() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _buildCreditCard(iconSize: 90, rate: 0.2),
+        _buildCreditCard(iconSize: 120, rate: 0.5),
+        _buildCreditCard(iconSize: 150, rate: 0.8),
+      ],
+    );
+  }
+
+  Widget _buildRatedFavorites() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: List.generate(5, (index) =>
+        BicolorIcon(
+          iconData: Icons.favorite,
+          iconSize: 50,
+          rate: min(1, max(0, (_currentSliderValue - index * 20) / 20)),
+          beginAlignment: Alignment.centerLeft,
+          beginColor: Colors.pink,
+          endColor: Colors.white
+        )
+      ),
+    );
+  }
+
+
+  Widget _buildSlider() {
+    return Slider(
+      value: _currentSliderValue,
+      min: 0,
+      max: 100,
+      divisions: 100,
+      label: _currentSliderValue.round().toString(),
+      activeColor: Colors.blueGrey,
+      inactiveColor: Colors.grey,
+      onChanged: (double value) {
+        setState(() {
+          _currentSliderValue = value;
+        });
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
-        ),
-      ),
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 5),
+            child: _buildFavorites()
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 5),
+            child: _buildStars()
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 5),
+            child: _buildCreditCards()
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 5),
+            child: _buildRatedFavorites()
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 5),
+            child: _buildSlider()
+          ),
+        ],
+      )
     );
   }
 }
